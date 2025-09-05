2a32301e2b784ab70c85f6d0735e1d0fc0321f6f
Lowe David Tubat <lowedavidctubat02@gmail.com>
2025-09-05 19:51:22 +0800

    Done doing Assignment: 12 Javascript-refresher

---
+++ 12_Javascript-Refresher/11_arrow_functions.js
@@ -0,0 +1,10 @@
+const greet = name => "Hello, " + name;
+
+const square = n => n * n;
+
+const sayHi = () => console.log("Hi!");
+
+
+console.log(greet("Lowe")); 
+console.log(square(5));    
+sayHi();                
