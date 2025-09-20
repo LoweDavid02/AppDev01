e27ffb11472ffb69cb51ab37f64a684c101c5282
Lowe David Tubat <lowedavidctubat02@gmail.com>
2025-09-20 14:20:24 +0800

    Done displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -42,3 +42,22 @@ function Avatar({ imageUrl, imageSize, name }) {
   );
 }
 
+// 4. Displaying data
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <Avatar
+        imageUrl={user.imageUrl}
+        imageSize={user.imageSize}
+        name={user.name}
+      />
+    </>
+  );
+}
