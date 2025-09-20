592e89bba76e75c1654da24b849e09edd33c5831
Lowe David Tubat <lowedavidctubat02@gmail.com>
2025-09-20 13:01:47 +0800

    Add classes and inheritance activity

---
+++ 12_javascript-refresher/14_classes_inheritance.js
@@ -0,0 +1,18 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  sayHello() {
+    console.log("Hi, I am " + this.name);
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(this.name + " is studying");
+  }
+}
+
+const student = new Student("Lowe");
+student.sayHello(); 
+student.study();   
