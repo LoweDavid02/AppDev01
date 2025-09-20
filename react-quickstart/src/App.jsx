1ddd5183fa6f0b6c3f86b2f76e24f96999d04f6f
Lowe David Tubat <lowedavidctubat02@gmail.com>
2025-09-20 14:16:54 +0800

    Done writing markup with JSX

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -17,4 +17,12 @@ function Welcome() {
   );
 }
 
-
+// 2. Writing markup with JSX
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
