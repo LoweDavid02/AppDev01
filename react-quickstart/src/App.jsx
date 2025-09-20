ce2596cca33b8d93fd55f899d59ea3eabf819242
Lowe David Tubat <lowedavidctubat02@gmail.com>
2025-09-20 14:22:11 +0800

    Done rendering lists

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -78,3 +78,25 @@ function ConditionalSection({ isLoggedIn }) {
   }
   return <div>{content}</div>;
 }
+
+// 6. Rendering lists
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+  return <ul>{listItems}</ul>;
+}
+
