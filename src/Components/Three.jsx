b85fb91a12d6f396ce5758ba6d81b954a744672e
CJayRomero <your_github_email@example.com>
2025-10-25 04:18:34 +0800

    Done adding a markup to javascript to JSX

---
+++ src/Components/Three.jsx
@@ -0,0 +1,9 @@
+export default function Three() {
+  const message = "JSX lets you write markup inside JavaScript!";
+  return (
+    <div>
+      <h3>JSX Example</h3>
+      <p>{message}</p>
+    </div>
+  );
+}
