81a60dc6065c143019e8896f02e245e563cdfe00
LoweDavid02 <louiecamarines21@gmail.com>
2025-11-09 23:49:27 +0800

    Done with Adding Recap topic component with summary bullets

---
+++ src/components/topics/Recap.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Recap() {
+  return (
+    <section>
+      <h3>Recap</h3>
+      <ul>
+        <li>Move small components to their own files.</li>
+        <li>Use default exports for single main exports.</li>
+        <li>Use named exports for multiple related exports.</li>
+      </ul>
+    </section>
+  )
+}
+s
