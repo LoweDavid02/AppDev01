8c844342afea7f59b68841cb3095b6653146503a
LoweDavid02 <louiecamarines21@gmail.com>
2025-11-10 10:47:02 +0800

    Done with Adding TodoList component with themed styling

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,17 +1,46 @@
 export default function TodoList() {
+  const hedy = {
+    name: "Hedy Lamarr",
+    img: "https://i.imgur.com/yXOvdOSs.jpg",
+    todos: [
+      "Invent new traffic lights",
+      "Rehearse a movie scene",
+      "Improve the spectrum technology",
+    ],
+  };
+
+  const gregorio = {
+    name: "Gregorio Y. Zara",
+    img: "https://i.imgur.com/7vQD0fPs.jpg",
+    theme: { backgroundColor: "black", color: "pink" },
+    todos: [
+      "Improve the videophone",
+      "Prepare aeronautics lectures",
+      "Work on the alcohol-fuelled engine",
+    ],
+  };
+
   return (
     <>
-      <h1>Hedy Lamarr's Todos</h1>
-      <img 
-        src="https://i.imgur.com/yXOvdOSs.jpg" 
-        alt="Hedy Lamarr" 
-        className="photo" 
-      />
+      {/* Hedy Section */}
+      <h1>{hedy.name}'s Todos</h1>
+      <img src={hedy.img} alt={hedy.name} className="photo" />
       <ul>
-        <li>Invent new traffic lights</li>
-        <li>Rehearse a movie scene</li>
-        <li>Improve the spectrum technology</li>
+        {hedy.todos.map((t, i) => (
+          <li key={i}>{t}</li>
+        ))}
       </ul>
+
+      {/* Gregorio Section */}
+      <div style={gregorio.theme}>
+        <h1>{gregorio.name}'s Todos</h1>
+        <img className="avatar" src={gregorio.img} alt={gregorio.name} />
+        <ul>
+          {gregorio.todos.map((t, i) => (
+            <li key={i}>{t}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
